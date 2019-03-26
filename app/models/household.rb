a4af0a8cae08e0cbc305212b2426361a7d1c04ce
Akihiro <kouen.xyz1@gmail.com>
2019-03-26 23:32:28 +0000

    昨日からの修正点

--- app/models/household.rb
+++ app/models/household.rb
@@ -1,6 +1,6 @@
 class Household < ApplicationRecord
   enum payment: { cash: 1, bank_account: 2, credit: 3, debit: 4, gift_card: 5, point: 6, others: 7 }
-  enum check_out: {receive: 1,pay: 2, complete_c: 3}
+  enum check_out: {receive: 1, pay: 2, complete_c: 3}
   enum detail: {incomplete: 1, complete_d: 2}
   enum analysis: {needs: 1,wants: 2}
 end
